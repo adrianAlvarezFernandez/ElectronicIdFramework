ec9c5ad25522b7862652146f3afeecc7ed2b4e49
Adrián Álvarez Fernández <adrian@electronicid.eu>
2016-06-23 09:23:25 +0200

    Initial Commit

---
+++ ElectronicIdFramework/ElectronicIdFramework.h
@@ -0,0 +1,19 @@
+//
+//  ElectronicIdFramework.h
+//  ElectronicIdFramework
+//
+//  Created by Adrián Álvarez Fernández on 23/6/16.
+//  Copyright © 2016 Adrián Álvarez Fernández. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElectronicIdFramework.
+FOUNDATION_EXPORT double ElectronicIdFrameworkVersionNumber;
+
+//! Project version string for ElectronicIdFramework.
+FOUNDATION_EXPORT const unsigned char ElectronicIdFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElectronicIdFramework/PublicHeader.h>
+
+
